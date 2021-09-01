e5761eb162303bb976836fbc1e99e0287a4702cd
cJeyB <88656451+cJeyB@users.noreply.github.com>
2021-09-02 00:04:49 +0200

    codereview fixes done

---
+++ event.js
@@ -0,0 +1,18 @@
+class Event {
+  constructor(name) {
+    this.name = name; // location must be added
+    this.acceptedBy = [];
+  } // participitans = [] must be added
+}
+
+module.exports = Event;
+
+// class Review {
+//     constructor(vet) {
+//         this.vet = vet
+//         this.reviewedBy = []
+//     }
+// }
+
+// start  + end event must be defined
+// participitans = [] must be added and pushed
