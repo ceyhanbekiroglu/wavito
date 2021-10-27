a3e4dd77a90ffceae85e85f02fed47dcc8253471
ceyhanbekiroglu <ceyhanbekiroglu@gmail.com>
2021-10-27 22:30:47 +0300

    Add live chat with socket.io

---
+++ frontend/src/views/profile.vue
@@ -0,0 +1,68 @@
+<script>
+// @ is an alias to /src
+import axios from 'axios'
+import UserCard from '@/components/user-card.vue'
+import Counter from '@/components/counter.vue'
+import { mapActions, mapState } from 'vuex'
+
+export default {
+  name: 'Profile',
+  components: {
+    UserCard,
+    Counter,
+  },
+  data() {
+    return {
+      users: [],
+      time: new Date(),
+      message: '',
+    }
+  },
+  async created() {
+    const usersRequest = await axios.get('/api/users')
+    this.users = usersRequest.data
+    // this.users = await this.fetchUsers()
+  },
+  methods: {
+    ...mapActions(['fetchUsers', 'goLive', 'sendMessageToLiveStream', 'joinStream']),
+    sendMessage(e) {
+      e.preventDefault()
+      this.sendMessageToLiveStream(this.message)
+      this.message = ''
+    },
+  },
+  computed: {
+    ...mapState(['currentLiveStream', 'liveStreams', 'user', 'liveStreamMessages']),
+  },
+}
+</script>
+
+<template lang="pug">
+  .home
+    h1 Wavito App 
+    h2 Welcome to your pets' best place {{user.name}} :)
+
+
+
+    p The time is: {{ time }}
+    div(v-for="user in users")
+      router-link(:to="`/users/${user._id}`") {{ user.name }}
+    Counter
+    div(v-if="liveStreams.length")
+      h2 Live streams
+      div(v-for="stream in liveStreams")
+        p {{ stream }}
+        button(@click="joinStream(stream)") Join stream
+    button(@click="goLive") Go live
+    div(v-if="currentLiveStream")
+      h3 Live stream
+      .messages
+        .message(v-for="message in liveStreamMessages")
+          p
+            span {{ message.author }}:&nbsp;
+            span {{ message.body }}
+      form(@submit="sendMessage")
+        input(type="text" v-model="message")
+        input(type="submit" value="Send message")
+
+</template>
