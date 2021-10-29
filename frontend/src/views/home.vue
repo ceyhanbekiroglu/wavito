373efa7166f854ecf2628e0988a499c0cf030bcf
ceyhanbekiroglu <ceyhanbekiroglu@gmail.com>
2021-10-29 18:08:15 +0300

    create home.vue

---
+++ frontend/src/views/home.vue
@@ -0,0 +1,9 @@
+<template lang="pug">
+    .home
+        h1 Wavito App 
+        h2 Welcome to your pets' best place :)
+        img(alt="Wavito home" width=500 src="../assets/wavito-sniffing.jpg")
+        h3 Missed your friends?  <router-link to="/login">Login</router-link>
+        h3 Let's find a play friend for you:) <router-link to="/register">Register</router-link>
+
+</template>
